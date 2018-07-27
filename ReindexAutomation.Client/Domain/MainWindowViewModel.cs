fad01cdcf46df76acbcfc9bdda6c75f61df4936f
Bazger <ivan.zolotaryov@gmail.com>
2018-07-28 02:01:09 +0300

    Binding upgrade

--- ReindexAutomation.Client/Domain/MainWindowViewModel.cs
+++ ReindexAutomation.Client/Domain/MainWindowViewModel.cs
@@ -20,7 +20,7 @@ namespace ReindexAutomation.Client.Domain
                         DocumentationLink.DemoPageLink<Home>()
                     }
                 ),
-                new Section("Zookeper managment", new ZookeeperManagment(),
+                new Section("Zookeper managment", new ZookeeperManagment{DataContext = new ZookeeperManagmentViewModel(snackbarMessageQueue)},
                     new[]
                     {
                         new DocumentationLink(DocumentationLinkType.Wiki,
--- ReindexAutomation.Client/Domain/ZookeeperManagmentViewModel.cs
+++ ReindexAutomation.Client/Domain/ZookeeperManagmentViewModel.cs
@@ -4,6 +4,9 @@ using MaterialDesignThemes.Wpf;
 using MaterialDesignThemes.Wpf.Transitions;
 using System.Windows.Controls;
 using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Input;
 
 namespace ReindexAutomation.Client.Domain
 {
@@ -11,6 +14,23 @@ namespace ReindexAutomation.Client.Domain
     {
         public ZookeeperManagmentViewModel(ISnackbarMessageQueue snackbarMessageQueue)
         {
+            ApplyCommand = new AnotherCommandImplementation(_ => GetFiles(ConfigsPath));
         }
+
+        public string ConfigsPath { get; set; }
+
+        public ICommand ApplyCommand { get; }
+
+        private void GetFiles(string path, int depth = 2)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }           
+            var a = Directory.GetDirectories(path).ToList();            
+            var b = a.Count();
+        }
+
+        public string SelectedConfig { get; set; }
     }
 }
